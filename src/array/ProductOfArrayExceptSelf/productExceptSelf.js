2f111b807380f8bdc18e587fda030db964d0a3fb
Brown <2brownit@gmail.com>
2022-12-30 11:13:17 +0530

    start with left sweep

--- src/array/ProductOfArrayExceptSelf/productExceptSelf.js
+++ src/array/ProductOfArrayExceptSelf/productExceptSelf.js
@@ -3,14 +3,14 @@ function productExceptSelf(nums) {
   let prefix = 1;
   let postfix = 1;
 
-  for (let i = nums.length - 1; i >= 0; i--) {
-    answer[i] = postfix;
-    postfix *= nums[i];
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = prefix;
+    prefix *= nums[i];
   }
 
-  for (let i = 0; i < nums.length; i++) {
-    answer[i] *= prefix;
-    prefix *= nums[i];
+  for (let i = nums.length - 1; i >= 0; i--) {
+    answer[i] *= postfix;
+    postfix *= nums[i];
   }
 
   return answer;
